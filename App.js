6b92a349d61484b02602efa16c57f824d9df2647
Wassimsghaier420 <52701776+Wassimsghaier420@users.noreply.github.com>
2019-09-12 00:27:52 +0100

    sixth commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import StartAndResetTimer from './ignition';
+
+
+function millisToMinutesAndSeconds(millis) {
+  var hours = Math.floor(millis / 1000 / 3600) % 24;
+  var minutes = Math.floor(millis / 60000) % 60;
+  var seconds = parseInt((millis / 1000) % 60);
+  return { hours, minutes, seconds };
+}
+
+let refInterval = [];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      millSecond: 1000,
+    }
+  }
+  Reset = () => {
+    refInterval.map(el => clearInterval(el))
+
+    this.setState({ millSecond: 0 });
+
+  }
+  Pause = () => {
+    clearInterval(refInterval)
+  }
+
+  IncrementItem = () => {
+    let incremCallBack = () => {
+      this.setState({ millSecond: this.state.millSecond + 1000 });
+    }
+    refInterval.push(setInterval(incremCallBack, 1000));
+
+  }
+
+  render() {
+    let { hours, minutes, seconds } = millisToMinutesAndSeconds(this.state.millSecond);
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return (
+      <div className="App" >
+        <div className="Title">
+
+          <h2 className='time'>{` ${hours} : ${minutes} : ${seconds}`} </h2>
+          <div className='teim'>
+          <label>Hour </label>
+          <label>Minute </label>
+          <label>seconds</label>
+          </div>
+        </div>
+        <StartAndResetTimer
+          handlerReset={this.Reset}
+          handlerInc={this.IncrementItem}
+          handlerPose={this.Pause} />
+
+      </div>
+    );
+  }
+
+}
+
+export default App;
